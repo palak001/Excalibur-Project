91d306c3a8af21347e17a57d03cefd079ab958d4
aarushivohra <aarushivohra01@gmail.com>
2020-11-22 14:34:14 +0530

    added file structure and initial location

---
+++ JS/accessToken.js
@@ -0,0 +1,7 @@
+mapboxgl.accessToken = "pk.eyJ1Ijoia3VtdWRzaGFuZGlseWEiLCJhIjoiY2tocHo4bW9mMG9odDJzcDVhbDZ5Ymw1MyJ9.-K1HV9V4_SBAvw-wQl3LTw";
+var map = new mapboxgl.Map({
+    container: "map", // container id
+    style: "mapbox://styles/mapbox/light-v10", // stylesheet location
+    center: [77.18831, 28.61787], // starting position
+    zoom: 12, // starting zoom
+}); 
